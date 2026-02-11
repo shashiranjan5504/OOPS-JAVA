d3ee994721c9d3517ae8ab3e93fdf8cc62dacf56
shashiranjan5504 <shashiranjan5504@gmail.com>
2026-02-12 00:00:41 +0530

    modify

--- Intro/Demo3.java
+++ Intro/Demo3.java
@@ -14,14 +14,17 @@ class mainClass3 {
     //main method
     public static void main(String[] args) {
         System.out.println("main method is running ");
-       // Demo3.test();
-       //Demo3.disp();
-        Demo1.x = 300;
-        Demo1.y = 400;
+        Demo3.test();
+        Demo3.disp();
+       
         System.out.println(Demo1.x);
         System.out.println(Demo1.y);
-        // System.out.println(Demo2.x);
-        // System.out.println(Demo2.y);
+
+        //here a copy of demo1 classs will come not the exact value so the  modification done in class2 is not visible in this class,the original value will print where  this variable define 
+        System.out.println(Demo2.x);
+        System.out.println(Demo2.y);
+        //here a copy of demo2 classs will come not the exact value so the  modification done in class2 is not visible in this class,the original value will print where  this variable define
+
         System.out.println("main method ends");
 
     }
